e7d445f6954f71b0303692cf2d60161edbcd5839
Helen <fitsumbrhanhelen@gmail.com>
2025-06-11 07:27:35 +0300

    Add README and 2-arguments.js

---
+++ 2-arguments.js
@@ -0,0 +1,11 @@
+const i = 0;
+const numberOfArgument = i - 2;
+if(numberOfArgument == 0){
+    console.log("No Argument");
+}
+else if(numberOfArgument == 1){
+    console.log("Argument found");
+}
+else{
+    console.log("Arguments found");
+}

47f9f8d82657a79b1504264fbed1278bed08914d
Helen <fitsumbrhanhelen@gmail.com>
2025-06-11 07:57:29 +0300

    README.md 2-arguments

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,11 +1,19 @@
-const i = 0;
-const numberOfArgument = i - 2;
-if(numberOfArgument == 0){
+function printMessageForArgumentCount(count){
+if(count === 0){
     console.log("No Argument");
+
 }
-else if(numberOfArgument == 1){
+else if(count === 1){
     console.log("Argument found");
+    
 }
 else{
     console.log("Arguments found");
 }
+}
+printMessageForArgumentCount(0); 
+
+printMessageForArgumentCount(1); 
+
+printMessageForArgumentCount(2); 
+
--- README.md
+++
@@ -1,3 +0,0 @@
-guillaume@ubuntu:~/0x12$ node 0-javascript_is_amazing.js 
-JavaScript is amazing
-guillaume@ubuntu:~/0x12$ 

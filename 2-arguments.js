ed5ac9be2fee7f14bbb3e02eedb7b0b4bf28ce10
Helen <fitsumbrhanhelen@gmail.com>
2025-06-11 20:40:21 +0300

    README.md 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -3,9 +3,9 @@ const numberOfArguments = process.argv.length - 2;
 // Use conditional statements (if/else if/else) to determine the message
 // based on the *actual* number of arguments received in this specific execution.
 
-if (numberOfArguments === 0) {
+if (numberOfArguments == 0) {
   console.log("No argument");
-} else if (numberOfArguments === 1) {
+} else if (numberOfArguments == 1) {
   console.log("Argument found");
 } else { // numberOfArguments > 1
   console.log("Arguments found");

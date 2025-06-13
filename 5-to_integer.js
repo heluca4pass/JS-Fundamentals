cf45eeb19d27967f9ad9170e03acd76a3a699306
Helen <fitsumbrhanhelen@gmail.com>
2025-06-13 05:12:25 +0300

    README.md 5-to_integer.js

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,10 +1,10 @@
-const numberOfArguments = process.argv.length - 2;
+//const numberOfArguments = process.argv.length - 2
 //const arg1 = process.argv[2];
-let num =parseInt(process.argv[2])
-if (Number.isNaN(num)) {
-  console.log("Not a number:");
+//let num =parseInt(process.argv[2])
+if (!Number.isNaN(parseInt(process.argv[2]))) {
+  console.log("My number: ", parseInt(process.argv[2]))
 } 
 
 else {
-    console.log("My number ", num);
+    console.log("Not a number")
 }

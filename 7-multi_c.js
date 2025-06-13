db67513edbb6438e5b879b1d1c738036f62a1370
Helen <fitsumbrhanhelen@gmail.com>
2025-06-13 05:58:12 +0300

    README.md 6-multi_languages_loop.js

---
+++ 7-multi_c.js
@@ -0,0 +1,9 @@
+const x = process.argv[2]
+if(!Number.isNaN(x)){
+    for(let i = 0; i<x; i++){
+        console.log('C is fun')
+    }
+}
+else{
+    console.log('Missing number of occurrences')
+}

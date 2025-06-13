cd0bed3555b3f7ecd5570897b4beb03d39fe2e4b
Helen <fitsumbrhanhelen@gmail.com>
2025-06-13 05:44:20 +0300

    README.md 6-multi_languages_loop.js

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,6 +1,4 @@
-arr = ["C is fun", "Python is cool", "JavaScript is amazing"]
-i=0
-while(i<arr.length){
-    console.log(arr[i])
-    i++
-}
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+    arr.forEach(item => {
+      console.log(item);
+    });
